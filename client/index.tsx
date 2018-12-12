ac358c95e530d40cbf8e1672f79332446bba8994
David Villegas <dville00@gmail.com>
2018-12-11 22:38:07 -0500

    Chapter10: Use React

---
+++ client/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles.css';
+
+import HelloWorld from './components/HelloWorld';
+
+ReactDOM.render(<HelloWorld>Hello World</HelloWorld>, document.getElementById('root'));
